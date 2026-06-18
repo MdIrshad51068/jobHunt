4cea7251693ea38431020878efbf1f892da318ab
MdIrshad51068 <mdirshad.cse2022@ritroorkee.com>
2026-06-18 21:57:48 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://jobhunt-r4iz.onrender.com',
+    origin:'https://jobhunt-wmd2.onrender.com',
     credentials:true
 }
 
